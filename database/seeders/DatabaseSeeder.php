7557b0ebc56e7a92c87bd00c4dbe3dab788d2e0d
muhammadhanskhaffah-ctrl <muhammadhanskhaffah@gmail.com>
2026-09-13 23:44:07 +0700

    Aktifkan seeder Goal dan Goal KPI

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             KaryawanSeeder::class,
+            GoalSeeder::class,
+            GoalKpiSeeder::class,
         ]);
     }
 }
